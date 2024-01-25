b4456075a83cd45978ec32b60b197f81d2ac6c44
Schlag <89420541+Schlagonia@users.noreply.github.com>
2024-01-24 19:22:14 -0700

    feat: apr oracle governed (#33)
    * feat: apr oracle governed
    
    * fix: deployment script

--- script/DeployAprOracle.s.sol
+++ script/DeployAprOracle.s.sol
@@ -6,19 +6,22 @@ import "forge-std/Script.sol";
 // Deploy a contract to a deterministic address with create2
 contract DeployAprOracle is Script {
 
-    Deployer public deployer = Deployer(0x8D85e7c9A4e369E53Acc8d5426aE1568198b0112);
+    Deployer public deployer = Deployer(0xba5Ed099633D3B313e4D5F7bdc1305d3c28ba5Ed);
 
     function run() external {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         vm.startBroadcast(deployerPrivateKey);
 
+        // Encode constructor arguments
+        bytes memory construct = abi.encode(0x33333333D5eFb92f19a5F94a43456b3cec2797AE);
+
         // Get the bytecode
-        bytes memory bytecode = abi.encodePacked(vm.getCode("AprOracle.sol:AprOracle"));
+        bytes memory bytecode =  abi.encodePacked(vm.getCode("AprOracle.sol:AprOracle"), construct);
 
         // Pick an unique salt
-        uint256 salt = uint256(keccak256("APR Oracle"));
+        bytes32 salt = keccak256("APR Oracle");
 
-        address contractAddress = deployer.deploy(bytecode, salt);
+        address contractAddress = deployer.deployCreate2(salt, bytecode);
 
         console.log("Address is ", contractAddress);
 
@@ -27,17 +30,10 @@ contract DeployAprOracle is Script {
 }
 
 contract Deployer {
-    event Deployed(address addr, uint256 salt);
+    event ContractCreation(address indexed newContract, bytes32 indexed salt);
 
-    function deploy(bytes memory code, uint256 salt) external  returns (address) {
-        address addr;
-        assembly {
-            addr := create2(0, add(code, 0x20), mload(code), salt)
-            if iszero(extcodesize(addr)) {
-                revert(0, 0)
-            }
-        }
-        emit Deployed(addr, salt);
-        return addr;
-    }
+    function deployCreate2(
+        bytes32 salt,
+        bytes memory initCode
+    ) public payable returns (address newContract) {}
 }
