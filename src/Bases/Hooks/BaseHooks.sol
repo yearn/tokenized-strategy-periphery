0b6cbc76ea5fbddd838b51516c8480054f07d1a3
Schlagonia <schlagonia@gmail.com>
2024-02-06 15:14:00 -0700

    chore: organize

--- src/Bases/Hooks/BaseHooks.sol
+++ src/Bases/Hooks/BaseHooks.sol
@@ -1,56 +1,8 @@
 // SPDX-License-Identifier: AGPL-3.0
 pragma solidity 0.8.18;
 
+import {Hooks} from "./Hooks.sol";
 import {BaseHealthCheck, ERC20} from "../HealthCheck/BaseHealthCheck.sol";
-
-contract DepositHooks {
-    function _preDepositHook(
-        uint256 assets,
-        uint256 shares,
-        address receiver
-    ) internal virtual {}
-
-    function _postDepositHook(
-        uint256 assets,
-        uint256 shares,
-        address receiver
-    ) internal virtual {}
-}
-
-contract WithdrawHooks {
-    function _preWithdrawHook(
-        uint256 assets,
-        uint256 shares,
-        address receiver,
-        address owner,
-        uint256 maxLoss
-    ) internal virtual {}
-
-    function _postWithdrawHook(
-        uint256 assets,
-        uint256 shares,
-        address receiver,
-        address owner,
-        uint256 maxLoss
-    ) internal virtual {}
-}
-
-contract TransferHooks {
-    function _preTransferHook(
-        address from,
-        address to,
-        uint256 amount
-    ) internal virtual {}
-
-    function _postTransferHook(
-        address from,
-        address to,
-        uint256 amount,
-        bool success
-    ) internal virtual {}
-}
-
-contract Hooks is DepositHooks, WithdrawHooks, TransferHooks {}
 
 /**
  *   @title Base Hooks
---
+++ src/Bases/Hooks/Hooks.sol
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: AGPL-3.0
+pragma solidity 0.8.18;
+
+contract Hooks is DepositHooks, WithdrawHooks, TransferHooks {}
+
+contract DepositHooks {
+    function _preDepositHook(
+        uint256 assets,
+        uint256 shares,
+        address receiver
+    ) internal virtual {}
+
+    function _postDepositHook(
+        uint256 assets,
+        uint256 shares,
+        address receiver
+    ) internal virtual {}
+}
+
+contract WithdrawHooks {
+    function _preWithdrawHook(
+        uint256 assets,
+        uint256 shares,
+        address receiver,
+        address owner,
+        uint256 maxLoss
+    ) internal virtual {}
+
+    function _postWithdrawHook(
+        uint256 assets,
+        uint256 shares,
+        address receiver,
+        address owner,
+        uint256 maxLoss
+    ) internal virtual {}
+}
+
+contract TransferHooks {
+    function _preTransferHook(
+        address from,
+        address to,
+        uint256 amount
+    ) internal virtual {}
+
+    function _postTransferHook(
+        address from,
+        address to,
+        uint256 amount,
+        bool success
+    ) internal virtual {}
+}
