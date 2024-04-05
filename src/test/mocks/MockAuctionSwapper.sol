7a55c061254a61510c3d991de43f3a45c18fa99d
Schlagonia <schlagonia@gmail.com>
2024-04-04 20:55:45 -0600

    fix: greater or equal

--- src/test/mocks/MockAuctionSwapper.sol
+++ src/test/mocks/MockAuctionSwapper.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-3.0
-pragma solidity 0.8.18;
+pragma solidity >=0.8.18;
 
 import {AuctionSwapper, SafeERC20} from "../../swappers/AuctionSwapper.sol";
 import {BaseStrategy, ERC20} from "@tokenized-strategy/BaseStrategy.sol";
--- src/test/mocks/MockHooks.sol
+++ src/test/mocks/MockHooks.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: AGPL-3.0
-pragma solidity 0.8.18;
+pragma solidity >=0.8.18;
 
 import {BaseHooks, ERC20} from "../../Bases/Hooks/BaseHooks.sol";
 
