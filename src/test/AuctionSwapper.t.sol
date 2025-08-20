a81494a084ecaafcf98e4044aeafe28fb0e1c716
Schlagonia <schlagonia@gmail.com>
2025-08-20 10:50:17 -0600

    test: auction swapper

--- src/test/AuctionSwapper.t.sol
+++ src/test/AuctionSwapper.t.sol
@@ -10,13 +10,8 @@ import {Auction, AuctionFactory} from "../Auctions/AuctionFactory.sol";
 contract AuctionSwapperTest is Setup {
     using SafeERC20 for ERC20;
 
-    event DeployedNewAuction(address indexed auction, address indexed want);
-
-    event AuctionEnabled(address indexed from, address indexed to);
-
-    event AuctionDisabled(address indexed from, address indexed to);
-
-    event AuctionKicked(address indexed token, uint256 available);
+    event AuctionSet(address indexed auction);
+    event UseAuctionSet(bool indexed useAuction);
 
     IMockAuctionSwapper public swapper;
 
@@ -38,112 +33,115 @@ contract AuctionSwapperTest is Setup {
         vm.label(address(swapper), "Auction Swapper");
     }
 
-    function test_enableAuction() public {
-        address from = tokenAddrs["USDC"];
+    function test_setAuction() public {
         assertEq(swapper.auction(), address(0));
+        assertFalse(swapper.useAuction());
 
-        swapper.enableAuction(from, address(asset));
+        // Create a new auction for testing
+        address newAuction = auctionFactory.createNewAuction(
+            address(asset),
+            address(swapper),
+            address(this),
+            1 days,
+            1e6
+        );
 
-        auction = Auction(swapper.auction());
-        assertNeq(address(auction), address(0));
+        // Setting auction should emit event
+        vm.expectEmit(true, false, false, false);
+        emit AuctionSet(newAuction);
+        swapper.setAuction(newAuction);
+
+        assertEq(swapper.auction(), newAuction);
+        assertFalse(swapper.useAuction());
+        
+        // Auction is set but useAuction is false, so kickable should be 0
+        address from = tokenAddrs["USDC"];
         assertEq(swapper.kickable(from), 0);
-        assertEq(auction.kickable(from), 0);
-        assertEq(auction.getAmountNeeded(from, 1e18), 0);
-        assertEq(auction.price(from), 0);
+        
+        // Enable an auction for the token
+        auction = Auction(newAuction);
+        auction.enable(from);
+        
+        // Still 0 because useAuction is false
+        assertEq(swapper.kickable(from), 0);
+    }
 
-        (uint64 _kicked, uint64 _scaler, uint128 _initialAvailable) = auction
-            .auctions(from);
+    function test_setUseAuction() public {
+        address from = tokenAddrs["USDC"];
+        
+        // Create and set auction
+        address newAuction = auctionFactory.createNewAuction(
+            address(asset),
+            address(swapper),
+            address(this),
+            1 days,
+            1e6
+        );
+        
+        swapper.setAuction(newAuction);
+        auction = Auction(newAuction);
+        auction.enable(from);
+        
+        assertFalse(swapper.useAuction());
+        assertEq(swapper.kickable(from), 0);
+        
+        // Set useAuction to true should emit event
+        vm.expectEmit(true, false, false, false);
+        emit UseAuctionSet(true);
+        swapper.setUseAuction(true);
+        
+        assertTrue(swapper.useAuction());
+        assertEq(swapper.kickable(from), 0); // Still 0 because no balance
+        
+        // Add some balance
+        airdrop(ERC20(from), address(swapper), 1000e6);
+        assertEq(swapper.kickable(from), 1000e6);
+        
+        // Set useAuction to false
+        vm.expectEmit(true, false, false, false);
+        emit UseAuctionSet(false);
+        swapper.setUseAuction(false);
+        
+        assertFalse(swapper.useAuction());
+        assertEq(swapper.kickable(from), 0); // Back to 0
+    }
 
-        assertEq(_kicked, 0);
-        assertEq(_scaler, 1e12);
-        assertEq(_initialAvailable, 0);
-        assertEq(auction.available(from), 0);
+    function test_setAuction_wrongReceiver() public {
+        // Create auction with wrong receiver
+        address wrongAuction = auctionFactory.createNewAuction(
+            address(asset),
+            address(this), // Wrong receiver (should be swapper)
+            address(this),
+            1 days,
+            1e6
+        );
+        
+        vm.expectRevert("wrong receiver");
+        swapper.setAuction(wrongAuction);
+    }
 
-        // Kicking it reverts
-        vm.expectRevert();
+    function test_kickAuction_withUseAuction_false() public {
+        address from = tokenAddrs["WBTC"];
+        
+        // Setup auction
+        address newAuction = auctionFactory.createNewAuction(
+            address(asset),
+            address(swapper),
+            address(this),
+            1 days,
+            1e6
+        );
+        swapper.setAuction(newAuction);
+        auction = Auction(newAuction);
+        auction.enable(from);
+        
+        // Add funds but don't enable useAuction
+        airdrop(ERC20(from), address(swapper), 1e8);
+        
+        vm.expectRevert("useAuction is false");
         swapper.kickAuction(from);
-
-        // Can't re-enable
-        vm.expectRevert("already enabled");
-        swapper.enableAuction(from, address(asset));
     }
-
-    function test_enableSecondAuction() public {
-        address from = tokenAddrs["USDC"];
-        assertEq(swapper.auction(), address(0));
-
-        swapper.enableAuction(from, address(asset));
-
-        auction = Auction(swapper.auction());
-
-        assertNeq(address(auction), address(0));
-        assertEq(swapper.kickable(from), 0);
-        assertEq(auction.kickable(from), 0);
-        assertEq(auction.getAmountNeeded(from, 1e18), 0);
-        assertEq(auction.price(from), 0);
-
-        (uint64 _kicked, uint64 _scaler, uint128 _initialAvailable) = auction
-            .auctions(from);
-
-        assertEq(_kicked, 0);
-        assertEq(_scaler, 1e12);
-        assertEq(_initialAvailable, 0);
-        assertEq(auction.available(from), 0);
-
-        address secondFrom = tokenAddrs["WETH"];
-
-        vm.expectRevert("wrong want");
-        swapper.enableAuction(secondFrom, from);
-
-        vm.expectEmit(true, true, true, true, address(auction));
-        emit AuctionEnabled(secondFrom, address(asset));
-        swapper.enableAuction(secondFrom, address(asset));
-
-        assertEq(swapper.auction(), address(auction));
-        assertEq(swapper.kickable(secondFrom), 0);
-        assertEq(auction.kickable(secondFrom), 0);
-        assertEq(auction.getAmountNeeded(secondFrom, 1e18), 0);
-        assertEq(auction.price(secondFrom), 0);
-        (_kicked, _scaler, _initialAvailable) = auction.auctions(secondFrom);
-
-        assertEq(_kicked, 0);
-        assertEq(_scaler, 1);
-        assertEq(_initialAvailable, 0);
-        assertEq(auction.available(secondFrom), 0);
-    }
-
-    function test_disableAuction() public {
-        address from = tokenAddrs["USDC"];
-        assertEq(swapper.auction(), address(0));
-        swapper.enableAuction(from, address(asset));
-
-        auction = Auction(swapper.auction());
-
-        assertNeq(address(auction), address(0));
-        assertEq(swapper.kickable(from), 0);
-        assertEq(auction.kickable(from), 0);
-        assertEq(auction.getAmountNeeded(from, 1e18), 0);
-        assertEq(auction.price(from), 0);
-        (uint64 _kicked, uint64 _scaler, uint128 _initialAvailable) = auction
-            .auctions(from);
-
-        assertEq(_kicked, 0);
-        assertEq(_scaler, 1e12);
-        assertEq(_initialAvailable, 0);
-        assertEq(auction.available(from), 0);
-
-        vm.expectEmit(true, true, true, true, address(auction));
-        emit AuctionDisabled(from, address(asset));
-        swapper.disableAuction(from);
-
-        (_kicked, _scaler, _initialAvailable) = auction.auctions(from);
-
-        assertEq(_kicked, 0);
-        assertEq(_scaler, 0);
-        assertEq(_initialAvailable, 0);
-        assertEq(auction.available(from), 0);
-    }
-
+    
     function test_kickAuction_default(uint256 _amount) public {
         vm.assume(_amount >= minFuzzAmount && _amount <= maxFuzzAmount);
 
@@ -152,9 +150,18 @@ contract AuctionSwapperTest is Setup {
         fromScaler = WAD / 10 ** ERC20(from).decimals();
         wantScaler = WAD / 10 ** ERC20(asset).decimals();
 
-        swapper.enableAuction(from, address(asset));
-
-        auction = Auction(swapper.auction());
+        // Setup auction properly
+        address newAuction = auctionFactory.createNewAuction(
+            address(asset),
+            address(swapper),
+            address(this),
+            1 days,
+            1e6
+        );
+        swapper.setAuction(newAuction);
+        swapper.setUseAuction(true);
+        auction = Auction(newAuction);
+        auction.enable(from);
 
         assertEq(swapper.kickable(from), 0);
         assertEq(auction.kickable(from), 0);
@@ -173,12 +180,12 @@ contract AuctionSwapperTest is Setup {
         assertEq(_kicked, 0);
         assertEq(_initialAvailable, 0);
 
-        uint256 available = swapper.kickAuction(from);
+        swapper.kickAuction(from);
 
         assertEq(ERC20(from).balanceOf(address(swapper)), 0);
         assertEq(ERC20(from).balanceOf(address(auction)), _amount);
 
-        assertEq(swapper.kickable(from), 0);
+        assertEq(swapper.kickable(from), _amount); // Now includes auction balance
         assertEq(auction.kickable(from), 0);
         (_kicked, , _initialAvailable) = auction.auctions(from);
         assertEq(_kicked, block.timestamp);
@@ -231,15 +238,24 @@ contract AuctionSwapperTest is Setup {
         fromScaler = WAD / 10 ** ERC20(from).decimals();
         wantScaler = WAD / 10 ** ERC20(asset).decimals();
 
-        swapper.enableAuction(from, address(asset));
-
-        auction = Auction(swapper.auction());
+        // Setup auction properly
+        address newAuction = auctionFactory.createNewAuction(
+            address(asset),
+            address(swapper),
+            address(this),
+            1 days,
+            1e6
+        );
+        swapper.setAuction(newAuction);
+        swapper.setUseAuction(true);
+        auction = Auction(newAuction);
+        auction.enable(from);
 
         airdrop(ERC20(from), address(swapper), _amount);
 
         swapper.kickAuction(from);
 
-        assertEq(swapper.kickable(from), 0);
+        assertEq(swapper.kickable(from), _amount); // Now includes auction balance
         (uint64 _kicked, uint64 _scaler, uint128 _initialAvailable) = auction
             .auctions(from);
         assertEq(_kicked, block.timestamp);
@@ -284,9 +300,18 @@ contract AuctionSwapperTest is Setup {
         fromScaler = WAD / 10 ** ERC20(from).decimals();
         wantScaler = WAD / 10 ** ERC20(asset).decimals();
 
-        swapper.enableAuction(from, address(asset));
-
-        auction = Auction(swapper.auction());
+        // Setup auction properly
+        address newAuction = auctionFactory.createNewAuction(
+            address(asset),
+            address(swapper),
+            address(this),
+            1 days,
+            1e6
+        );
+        swapper.setAuction(newAuction);
+        swapper.setUseAuction(true);
+        auction = Auction(newAuction);
+        auction.enable(from);
 
         assertEq(swapper.kickable(from), 0);
         (uint64 _kicked, uint64 _scaler, uint128 _initialAvailable) = auction
@@ -310,7 +335,7 @@ contract AuctionSwapperTest is Setup {
         assertEq(_kicked, 0);
         assertEq(_initialAvailable, 0);
 
-        uint256 available = swapper.kickAuction(from);
+        swapper.kickAuction(from);
 
         assertEq(ERC20(from).balanceOf(address(swapper)), _amount - kickable);
         assertEq(ERC20(from).balanceOf(address(auction)), kickable);
@@ -366,9 +391,18 @@ contract AuctionSwapperTest is Setup {
         fromScaler = WAD / 10 ** ERC20(from).decimals();
         wantScaler = WAD / 10 ** ERC20(asset).decimals();
 
-        swapper.enableAuction(from, address(asset));
-
-        auction = Auction(swapper.auction());
+        // Setup auction properly
+        address newAuction = auctionFactory.createNewAuction(
+            address(asset),
+            address(swapper),
+            address(this),
+            1 days,
+            1e6
+        );
+        swapper.setAuction(newAuction);
+        swapper.setUseAuction(true);
+        auction = Auction(newAuction);
+        auction.enable(from);
 
         airdrop(ERC20(from), address(swapper), _amount);
 
@@ -415,5 +449,98 @@ contract AuctionSwapperTest is Setup {
         assertEq(ERC20(from).balanceOf(address(auction)), left);
         assertEq(ERC20(asset).balanceOf(address(swapper)), needed);
         assertEq(ERC20(asset).balanceOf(address(auction)), 0);
+    }
+    
+    function test_kickAuction_activeAuction_withAvailable() public {
+        address from = tokenAddrs["WBTC"];
+        
+        // Setup auction
+        address newAuction = auctionFactory.createNewAuction(
+            address(asset),
+            address(swapper),
+            address(this),
+            1 days,
+            1e6
+        );
+        swapper.setAuction(newAuction);
+        swapper.setUseAuction(true);
+        auction = Auction(newAuction);
+        auction.enable(from);
+        
+        airdrop(ERC20(from), address(swapper), 1e8);
+        
+        // Kick the first auction
+        uint256 kicked = swapper.kickAuction(from);
+        assertTrue(kicked > 0);
+        assertTrue(Auction(auction).isActive(from));
+        assertTrue(Auction(auction).available(from) > 0);
+        
+        // Add more tokens
+        airdrop(ERC20(from), address(swapper), 1e8);
+        
+        // Trying to kick again should return 0 since auction is active and has available tokens
+        uint256 kicked2 = swapper.kickAuction(from);
+        assertEq(kicked2, 0);
+    }
+    
+    function test_kickAuction_activeAuction_noAvailable() public {
+        address from = tokenAddrs["WBTC"];
+        
+        // Setup auction
+        address newAuction = auctionFactory.createNewAuction(
+            address(asset),
+            address(swapper),
+            address(this),
+            1 days,
+            1e6
+        );
+        swapper.setAuction(newAuction);
+        swapper.setUseAuction(true);
+        auction = Auction(newAuction);
+        auction.enable(from);
+        
+        uint256 amount = 1e8;
+        airdrop(ERC20(from), address(swapper), amount);
+        
+        // Kick the first auction
+        swapper.kickAuction(from);
+        assertTrue(Auction(auction).isActive(from));
+        
+        // Take the entire auction
+        skip(auction.auctionLength() / 2);
+        uint256 needed = auction.getAmountNeeded(from, amount);
+        airdrop(ERC20(asset), address(this), needed);
+        ERC20(asset).forceApprove(address(auction), needed);
+        auction.take(from, amount);
+        
+        // Now available should be 0
+        assertEq(Auction(auction).available(from), 0);
+        
+        // Add more tokens and try to kick again - should settle and start new auction
+        airdrop(ERC20(from), address(swapper), amount);
+        uint256 kicked = swapper.kickAuction(from);
+        assertTrue(kicked > 0);
+        assertEq(ERC20(from).balanceOf(address(auction)), amount);
+    }
+    
+    function test_kickAuction_noBalance() public {
+        address from = tokenAddrs["WBTC"];
+        
+        // Setup auction
+        address newAuction = auctionFactory.createNewAuction(
+            address(asset),
+            address(swapper),
+            address(this),
+            1 days,
+            1e6
+        );
+        swapper.setAuction(newAuction);
+        swapper.setUseAuction(true);
+        auction = Auction(newAuction);
+        auction.enable(from);
+        
+        // No balance - should revert with "nothing to kick"
+        vm.expectRevert("nothing to kick");
+        swapper.kickAuction(from);
     }
 }
