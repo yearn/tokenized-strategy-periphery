96a3ac6c0732326e02bf297f295454fc4aa60f81
Schlagonia <schlagonia@gmail.com>
2025-09-15 11:22:06 -0600

    chore: deploy

--- script/DeployCommonTrigger.s.sol
+++ script/DeployCommonTrigger.s.sol
@@ -1,21 +1,19 @@
 // SPDX-License-Identifier: AGPL-3.0
 pragma solidity >=0.8.18;
 
-import "forge-std/Script.sol";
+import "./BaseScript.s.sol";
 
 // Deploy a contract to a deterministic address with create2
-contract DeployCommonTrigger is Script {
-
-    Deployer public deployer = Deployer(0xba5Ed099633D3B313e4D5F7bdc1305d3c28ba5Ed);
+contract DeployCommonTrigger is BaseScript {
 
     function run() external {
         vm.startBroadcast();
 
         // Encode constructor arguments
-        bytes memory construct = abi.encode(0x6f3cBE2ab3483EC4BA7B672fbdCa0E9B33F88db8);
+        bytes memory construct = abi.encode(initGov);
         
         // Append constructor args to the bytecode
-        bytes memory bytecode = abi.encodePacked(vm.getCode("CommonTrigger.sol:CommonTrigger"), construct);
+        bytes memory bytecode = abi.encodePacked(vm.getCode("src/ReportTrigger/CommonTrigger.sol:CommonTrigger"), construct);
 
         // Use 0 as salt.
         bytes32 salt;
@@ -26,13 +24,4 @@ contract DeployCommonTrigger is Script {
 
         vm.stopBroadcast();
     }
-}
-
-contract Deployer {
-    event ContractCreation(address indexed newContract, bytes32 indexed salt);
-
-    function deployCreate2(
-        bytes32 salt,
-        bytes memory initCode
-    ) public payable returns (address newContract) {}
 }
