c1dd69a45d5b07105a525d6315b92218c83938d6
johnnyonline <johnnyonline.eth@gmail.com>
2025-11-28 16:05:49 -0300

    fix: https://github.com/yearn/tokenized-strategy-periphery/pull/87#discussion_r2572309615

--- src/Auctions/Auction.sol
+++ src/Auctions/Auction.sol
@@ -32,7 +32,7 @@ contract Auction is Governance2Step, ReentrancyGuard {
     event AuctionKicked(address indexed from, uint256 available);
 
     /// @notice Emitted when the receiver is updated.
-    event UpdatedReceiver(address receiver);
+    event UpdatedReceiver(address indexed receiver);
 
     /// @notice Emitted when the minimum price is updated.
     event UpdatedMinimumPrice(uint256 minimumPrice);
