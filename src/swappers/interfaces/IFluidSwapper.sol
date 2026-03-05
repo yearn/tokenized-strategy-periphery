cde4c5885cf3f6b48629a85a498c715abac1f4d3
Schlagonia <schlagonia@gmail.com>
2026-03-05 09:27:44 -0700

    Revert "chore: remove Fluid swapper from convertor branch"
    This reverts commit 1794ac6179c2f6dff842aa0c8ba5d2f74604881f.

---
+++ src/swappers/interfaces/IFluidSwapper.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: AGPL-3.0
+pragma solidity >=0.8.18;
+
+import "./IBaseSwapper.sol";
+
+interface IFluidSwapper is IBaseSwapper {
+    function weth() external view returns (address);
+
+    function base() external view returns (address);
+
+    function fluidDexes(
+        address,
+        address
+    ) external view returns (address dex, bool swap0to1);
+}
